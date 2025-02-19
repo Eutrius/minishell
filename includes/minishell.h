81ba2ac63d6d0feca1eb3ad0d67c9b9d613c1ef5
Lorenzo Nulli <lonulli@e3r7p1.42roma.it>
2025-02-19 11:38:30 +0100

    Fixed conflicts

--- includes/minishell.h
+++ includes/minishell.h
@@ -34,7 +34,6 @@ typedef struct s_token
 
 }						t_token;
 
-
 t_token					**parse_cmd(char *cmd);
 char					*pathfinder(const char *cmd, char **env);
 void					print_tokens(t_token **tokens);
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,37 +3,25 @@
 #include <readline/history.h>
 #include <readline/readline.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int	main(void)
 {
 	char	*buf;
 	t_data	data;
-	t_token	**tokens;
 
 	(void)data;
 	while (1)
 	{
 		buf = readline("B_Bros > ");
 		add_history(buf);
-		tokens = parse_cmd(buf);
-		print_tokens(tokens);
-		custom_echo(buf, tokens[0]->content);
-		clean_exit(buf);
-		custom_pwd(buf);
-		custom_chdir(buf, tokens[0]->content);
-		free(tokens);
-
-
-	while (1)
-	{
-		buf = readline("B_bros > ");
-		if (!buf || !ft_strcmp(buf, "exit"))
-			exit(0);
-		add_history(buf);
 		data.cmd = parse_cmd(buf);
 		print_tokens(data.cmd);
+		custom_echo(buf, data.cmd[0]->content);
+		clean_exit(buf);
+		custom_pwd(buf);
+		custom_chdir(buf, data.cmd[0]->content);
 		free(buf);
+		return (0);
 	}
-	return (0);
 }
