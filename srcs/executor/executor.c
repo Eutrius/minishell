c7df8856833667f6344f5f2c2365c6e98fa21259
Lorenzo Nulli <lonulli@e3r7p1.42roma.it>
2025-02-27 18:18:56 +0100

    Started executor

---
+++ srcs/executor/executor.c
@@ -0,0 +1,40 @@
+#include "libft.h"
+#include "minishell.h"
+// clang-format off
+#include <readline/history.h>
+#include <readline/readline.h>
+// clang-format on
+#include <unistd.h>
+
+void execute_cmd(char **args, t_token *cmd, t_data *data);
+static int redirect_in(t_token *redirect);
+static int redirect_out(t_token *redirect);
+
+char **fill_args_array(t_token *cmd, t_data *data)
+{
+	int i = 0;
+	int idx =  cmd->index;
+	t_token **cmd_array = data->cmd_line;
+	i = idx;
+	while (cmd_array[i] && (cmd_array[i] & CMD))
+		i++;
+	char **args = ft_calloc(i - idx + 1, sizeof(char *));
+	if (!args)
+		return NULL;
+	i = 0;
+	while(cmd_array[idx] && (cmd_array[idx] & CMD))
+	{
+		args[i] = cmd_array[idx]->content;
+		idx++;
+		i++;
+	}
+	return args;
+}
+
+void execute_cmd(char **args,t_token *cmd, t_data *data)
+{
+	pid_t pid;
+	extern char **environ;
+	char *cmd_path;
+	cmd_path = pathfinder(cmd[0].content, char **env)
+}
